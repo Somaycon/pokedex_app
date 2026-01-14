d058902f8fb56637ed9297fbd39a696d285a65d7
somaycon <contato.somaycon@gmail.com>
2026-01-14 14:44:32 -0300

    feat: Adiciona o  modulo Home e Dio para ingerção de dependência

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,10 +1,19 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pokedex_app/features/home/home_module.dart';
 import 'package:pokedex_app/features/splash/splash_module.dart';
 
 class AppModule extends Module {
   @override
+  void binds(Injector i) {
+    i.addLazySingleton<Dio>(() => Dio());
+    super.binds(i);
+  }
+
+  @override
   void routes(RouteManager r) {
     r.module('/', module: SplashModule());
+    r.module('/home', module: HomeModule());
     super.routes(r);
   }
 }
